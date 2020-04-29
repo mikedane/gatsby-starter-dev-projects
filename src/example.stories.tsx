5c31cddd976a84166608969dcfb7a7ce73110d3a
mikedane <mikedane94@gmail.com>
2020-04-29 15:16:42 +1000

    added storybook

---
+++ src/example.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+const story = storiesOf('Components|Example', module)
+
+story.add('Example Component', () => <div>Hello World</div>);
